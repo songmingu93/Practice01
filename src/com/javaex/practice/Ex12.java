77acdb5c3dbbb0e8959018ce95963d4928f4ac92
thdalsrb93 <thdalsrb93@naver.com>
2021-05-31 01:45:46 +0900

    5월31

--- src/com/javaex/practice/Ex12.java
+++ src/com/javaex/practice/Ex12.java
@@ -1,10 +1,24 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double won, dollar;
+		final double Exchange = 1230.95;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("환전할 원화를 입력하세요 : ");
+		
+		won = sc.nextDouble();
+		dollar = won / Exchange;
+		
+		System.out.println("받으실 달러는 "+ dollar);
+
+		sc.close();
+		}
 
 	}
-
-}
